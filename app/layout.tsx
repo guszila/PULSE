fb5dcd533ff8c6bd52ea96eac75231c19a7a68a5
guszila <154215822+guszila@users.noreply.github.com>
2026-06-27 16:12:51 +0700

    Enhance PWA viewport for native feel

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export const viewport: Viewport = {
   themeColor: "#070807",
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover"
 };
 
 export default async function RootLayout({
